4ba2dfeba294851ff442131850659acd0d8348e9
Anand Raj <anandrajj10@gmail.com>
2023-09-01 09:13:16 +0530

    typescript file added

---
+++ typescript.js
@@ -0,0 +1,4 @@
+"use strict";
+/*TypeScript is JavaScript with added syntax for types ,
+or syntactic superset of JavaScript which adds static typing.*/
+console.log("Typescript");
---
+++ typescript.ts
@@ -0,0 +1,3 @@
+/*TypeScript is JavaScript with added syntax for types ,
+or syntactic superset of JavaScript which adds static typing.*/
+console.log("Typescript");
